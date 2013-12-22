b47cde12388f70f10c55920955829d94a7847e40
Florian Kraft <f.kraft@ewerk.com>
2013-12-22 18:50:07 +0100

    initial commit, torqueboxified consulted.co based on JRuby and MongoDB

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.2'
+
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'rabl'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+group :server do
+  gem "torquebox", "3.0.1"
+  gem "torquebox-server", "3.0.1"
+end
+
+group :development do |variable|
+  gem "pry", ">= 0.9.12.4"
+  gem "pry-rails"
+end
+
+group :db do
+  gem "mongoid", "~> 4.0.0.alpha1"
+  gem "bson"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Consulted::Application.config.secret_key_base = 'dd6fe1a1784a861750a2cd3ca72cb66fb40aae2a954ad71112e5f0a94f2a55e7506729feccfe56023915adf99efcea9af39bdc51a5cbbf7a9ff8c5e2f8039231'
