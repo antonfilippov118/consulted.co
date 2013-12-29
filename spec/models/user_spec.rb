3bdecbcdcc131e5487cf4e2f2a7cf455f28ed0dc
Florian Kraft <f.kraft@ewerk.com>
2013-12-30 00:28:01 +0100

    [refactor] make tests nicer to maintain

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,29 +9,23 @@ end
 
 describe User do
   context "create" do
-    it "can be assigned a name" do
-      u = User.new
-      u.name = "Florian"
+    [:name, :email].each do |sym|
+      it "can be assigned a #{sym}" do
+        u = User.new
 
-      expect(u.name).to eql "Florian"
-    end
+        u.send "#{sym}=", "test"
 
-    it "can be initialized with a user name" do
-      u = User.new name: "Florian"
+        expect(u.send(sym)).to eql "test"
+      end
 
-      expect(u.name).to eql "Florian"
-    end
+      it "can be initialized with a user name" do
+        params = {
+          sym => "test"
+        }
+        u = User.new params
 
-    it "can be assigned an email" do
-      u = User.new
-      u.email =  "florian@consulted.co"
-      expect(u.email).to eql "florian@consulted.co"
-    end
-
-    it "can be initialized with an email" do
-      u = User.new email: "florian@consulted.co"
-
-      expect(u.email).to eql "florian@consulted.co"
+        expect(u.send(sym)).to eql "test"
+      end
     end
   end
 end
