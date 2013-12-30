711949b8972963239e89ac920dbc6db7797b4bdd
Florian Kraft <f.kraft@ewerk.com>
2013-12-30 11:07:59 +0100

    [refactor] put user model in correct place

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,5 @@
 require "spec_helper"
 
-class User
-  include Mongoid::Document
-
-  [:name, :email, :telephone].each do |_field|
-    field _field, type: String
-  end
-
-  [:name, :email].each do |_field|
-    validates_presence_of _field
-  end
-
-  validates_uniqueness_of :email
-end
-
 describe User do
   context "create" do
     [:name, :email, :telephone].each do |sym|
