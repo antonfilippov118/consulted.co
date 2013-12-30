ac000da5c54616c0029170c63dff4a048f240a1c
Florian Kraft <f.kraft@ewerk.com>
2013-12-30 17:05:44 +0100

    [red] fail user creation to controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,41 @@ require 'spec_helper'
 
 describe UsersController do
   describe "POST #user" do
+    let :valid_params do
+      {
+        name: "Florian",
+        email: "Florian@consulted.co",
+        password: "tester",
+        password_confirmation: "tester"
+      }
+    end
+
+    let :invalid_params do
+      {
+        name: "Florian",
+        email: "Florian@consulted.co",
+        password_confirmation: "tester"
+      }
+    end
+
+    let(:user_class) { User }
+
+    before(:each) do
+      user_class.delete_all
+    end
+
     it "responds with a bad request when accessed without data" do
       post :create
       expect(response).not_to be_success
       expect(response.status).to eql 400
+    end
+
+    it "creates a user when accessed with valid data" do
+      post :create, valid_params
+      expect(response).to be_success
+      expect(response.status).to eql 201
+
+      expect(user_class.count).to eql 1
     end
   end
 end
