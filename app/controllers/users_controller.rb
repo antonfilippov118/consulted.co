949c92fb659b4e89bdd7d302fe864a828cfaef2e
Florian Kraft <f.kraft@ewerk.com>
2013-12-30 17:18:53 +0100

    [green] pass user creation through controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
   def create
-    head :bad_request
+    @user = user_class.new user_params
+    result = RegistersUser.for_new @user
+
+    if result.failure?
+      head :bad_request
+    else
+      head :created
+    end
+  end
+
+private
+  def user_class
+    User
+  end
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
