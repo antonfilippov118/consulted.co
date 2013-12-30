d8a2f6ccbb9cd2641529f79c210f034284d71097
Florian Kraft <f.kraft@ewerk.com>
2013-12-30 11:41:41 +0100

    [green] pass first registration test

--- spec/actions/registers_user_spec.rb
+++ spec/actions/registers_user_spec.rb
@@ -1,7 +1,21 @@
-class RegistersUser
+require "spec_helper"
 
+class RegistersUser
+  include LightService::Organizer
+  def self.for_new(user)
+    with(user: user).reduce [
+
+    ]
+  end
 end
 
 describe RegistersUser do
+  it "should be successful if user is valid" do
+    result = RegistersUser.for_new valid_user
+    expect(result.success?).to be_true
+  end
 
+  def valid_user
+    User.new name: "Florian", email: "florian@consulted.co", password: "tester"
+  end
 end
