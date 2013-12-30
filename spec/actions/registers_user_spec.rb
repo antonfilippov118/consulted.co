6c17022486cd56947610352ee4a9dc1b7bb2af9c
Florian Kraft <f.kraft@ewerk.com>
2013-12-30 13:04:25 +0100

    [red] fail user  email validation test in register user step

--- spec/actions/registers_user_spec.rb
+++ spec/actions/registers_user_spec.rb
@@ -29,11 +29,11 @@ describe RegistersUser do
     expect(result.failure?).to be_true
   end
 
-  # it "should fail when a user does not give a correct email address" do
-  #   user = user_class.new name: "Florian", email: "florian@.co", password: "tester", password_confirmation: "tester"
-  #   result = RegistersUser.for_new user
-  #   expect(result.failure?).to be_true
-  # end
+  it "should fail when a user does not give a correct email address" do
+    user = user_class.new name: "Florian", email: "florian@.co", password: "tester", password_confirmation: "tester"
+    result = RegistersUser.for_new user
+    expect(result.failure?).to be_true
+  end
 
   def valid_user
     user_class.new name: "Florian", email: "florian@consulted.co", password: "tester", password_confirmation: "tester"
