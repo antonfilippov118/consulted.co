834ef9fbcf9e3d6e53f511a9c9fc5c016ecab4c8
Florian Kraft <f.kraft@ewerk.com>
2013-12-30 12:49:44 +0100

    add validator folders and spec

---
+++ spec/validators/email_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+module Validators
+  class Email < ActiveModel::Validator
+    def validate(record)
+      record.errors.add :email, "is not properly formatted!"
+    end
+
+  private
+    def regexp
+      /.*/
+    end
+  end
+end
+
+describe Validators::Email do
+
+  let :validator do
+    Validators::Email.new
+  end
+
+  it "passes users with a valid email" do
+    validator.validate user_with_valid_email
+
+    expect(user_with_valid_email.errors).to be_empty
+  end
+
+  def user_with_valid_email
+    User.new email: "florian@consulted.co"
+  end
+
+  def user_with_invalid_email
+    User.new email: "florian@"
+  end
+
+  def user_with_empty_email
+    User.new
+  end
+end
