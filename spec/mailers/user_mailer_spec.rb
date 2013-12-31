52575a3ebb38ec8f91d49d1736b4742e02d3953b
Florian Kraft <f.kraft@ewerk.com>
2013-12-31 19:02:28 +0100

    [refactor] make field tests easier to design

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,20 +1,19 @@
 require "spec_helper"
 
 describe UserMailer do
+
   describe "activation" do
-    let(:user) { user_class.new name: "Florian", email: "FlorianKraft@gmx.de" }
+    user = User.new name: "Florian", email: "FlorianKraft@gmx.de"
     let(:mail) { UserMailer.activation user }
 
-    it "renders the subject properly" do
-      expect(mail.subject).to eql "Your consulted.co profile activation"
-    end
-
-    it "uses the user's email as an address" do
-      expect(mail.to).to eql [user.email]
+    {
+      subject: "Your consulted.co profile activation",
+      to: [user.email]
+    }.each_pair do |field, value|
+      it "should correctly set the value of #{field}" do
+        expect(mail.send field).to eql value
+      end
     end
   end
 
-  def user_class
-    User
-  end
 end
