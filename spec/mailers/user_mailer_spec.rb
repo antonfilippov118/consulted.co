e121c2635fa128c697c0d2798897594347c1fc2e
Florian Kraft <f.kraft@ewerk.com>
2013-12-31 18:38:53 +0100

    [red] fail mail subject test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 describe UserMailer do
+  describe "activation" do
+    let(:user) { user_class.new name: "Florian", email: "FlorianKraft@gmx.de" }
+    let(:mail) { UserMailer.activation user }
+  end
 
+  it "renders the subject properly" do
+    binding.pry
+    expect(mail.subject).to eql "Your consulted.co profile activation"
+  end
+
+  def user_class
+    User
+  end
 end
