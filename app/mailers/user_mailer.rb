d33a18c544f5725528ff776544bb7162dce4f726
Florian Kraft <f.kraft@ewerk.com>
2014-01-04 17:14:52 +0100

    [green] pass user name test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ActionMailer::Base
 
   def activation(user)
+    @user = user
     mail to: user.email, subject: "Your consulted.co profile activation", from: "registration@consulted.co"
   end
 
