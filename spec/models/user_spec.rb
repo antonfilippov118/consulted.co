e104eff26c16003d100b2a8807f1e5066a7b910e
Florian Kraft <f.kraft@ewerk.com>
2014-01-04 20:45:47 +0100

    [refactor] use subject for model testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,34 +2,26 @@
 require 'spec_helper'
 
 describe User do
+  subject(:user) { User.new }
   context 'create' do
-    [:name, :email, :telephone].each do |sym|
+    [:name, :email, :telephone, :single_access_token].each do |sym|
       it "can be assigned a #{sym}" do
-        u = User.new
-
-        u.send "#{sym}=", 'test'
-
-        expect(u.send(sym)).to eql 'test'
+        user.send "#{sym}=", 'test'
+        expect(user.send(sym)).to eql 'test'
       end
 
       it 'can be initialized with a user name' do
-        params = {
-          sym => 'test'
-        }
-        u = User.new params
+        user = User.new sym => 'test'
 
-        expect(u.send(sym)).to eql 'test'
+        expect(user.send(sym)).to eql 'test'
       end
     end
 
     it 'is created as an unconfirmed user' do
-      user = User.new
-
       expect(user.confirmed?).to be_false
     end
 
     it 'is created as an active user' do
-      user = User.new
       expect(user.active?).to be_true
     end
   end
@@ -44,8 +36,7 @@ describe User do
 
   context 'authentication' do
     it 'should make use of secure password' do
-      u = User.new
-      expect { u.password = 'foo' }.not_to raise_error
+      expect { user.password = 'foo' }.not_to raise_error
     end
   end
 end
