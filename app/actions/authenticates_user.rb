b365b3117b0e56c4b48421410ead20471cf30753
Florian Kraft <f.kraft@ewerk.com>
2014-01-05 18:43:25 +0100

    [green] pass user password authentication

--- app/actions/authenticates_user.rb
+++ app/actions/authenticates_user.rb
@@ -6,7 +6,8 @@ class AuthenticatesUser
 
   def self.check(data)
     with(data: data).reduce [
-      CheckExistenceAction
+      CheckExistenceAction,
+      CheckPasswordAction
     ]
   end
 
@@ -20,6 +21,24 @@ class AuthenticatesUser
       rescue => e
         context.set_failure! e
       end
+    end
+  end
+
+  class CheckPasswordAction
+    include LightService::Action
+
+    executed do |context|
+      user = context.fetch :user
+      data = context.fetch :data
+      begin
+        result = user.authenticate data[:password]
+        fail UserPasswordException if result == false
+      rescue => e
+        context.set_failure! e
+      end
+    end
+
+    class UserPasswordException < StandardError
     end
   end
 end
