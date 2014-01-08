696217ba616a73a6e6ebcd9ee392a49664d463d6
Florian Kraft <f.kraft@ewerk.com>
2014-01-08 19:08:49 +0100

    increase debug level in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,5 +24,8 @@ Consulted::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+
+  Mongoid.logger.level = Logger::DEBUG
+  Moped.logger.level   = Logger::DEBUG
 end
