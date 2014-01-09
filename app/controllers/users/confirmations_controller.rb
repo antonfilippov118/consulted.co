a46acff5dd949ee222f0bc87cb105dac3ed8d2c6
Florian Kraft <f.kraft@ewerk.com>
2014-01-09 12:26:53 +0100

    [green]

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,19 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  respond_to :json
+
   # resend confirmation
   def create
     head :method_not_allowed
+  end
+
+  # confirm user
+  def show
+    resource = User.confirm_by_token(params[:confirmation_token])
+
+    if resource.errors.empty?
+      render json: { success: true }
+    else
+      render json: { success: false }, status: 400
+    end
   end
 end
--- spec/controllers/users/confirmations_controller_spec.rb
+++ spec/controllers/users/confirmations_controller_spec.rb
@@ -11,14 +11,31 @@ describe Users::ConfirmationsController do
     expect(response).not_to be_success
   end
 
-  it 'should cofirm a user' do
-    user = User.create email: 'florian@consulted.co', password: 'tester', password_confirmation: 'tester'
-    expect(user.confirmed?).to be_false
+  it 'should confirm a user' do
+    token = create_new_user_and_send_email
 
-    get :show, confirmation_token: user.confirmation_token
+    get :show, confirmation_token: token
 
     expect(response).to be_success
     expect(User.first.confirmed?).to be_true
     expect(response.status).to eql 200
+  end
+
+  it 'should not confirm a user with an invalid token' do
+    create_new_user_and_send_email
+    get :show, confirmation_token: 'foo'
+
+    expect(response).not_to be_success
+    expect(response.status).to eql 400
+  end
+
+  def create_new_user_and_send_email
+    User.create email: 'florian@consulted.co', password: 'tester', password_confirmation: 'tester', confirmation_sent_at: 1.day.ago, unconfirmed_email: 'florian@consulted.co'
+
+    # fetch the token from the email just sent
+    body = ActionMailer::Base.deliveries.last.body.encoded
+
+    match = body.scan(/\?confirmation\_token=.*\"/).first
+    match.gsub('?confirmation_token=', '').gsub('"', '')
   end
 end
