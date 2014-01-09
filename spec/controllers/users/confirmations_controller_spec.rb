afb0775473e5a9d12140d0f96255e4b04303ff0e
Florian Kraft <f.kraft@ewerk.com>
2014-01-09 10:40:22 +0100

    [red]

--- spec/controllers/users/confirmations_controller_spec.rb
+++ spec/controllers/users/confirmations_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Users::ConfirmationsController do
-
+  it 'should create a bad request when accessed with nothing' do
+    post :create
+    expect(response).not_to be_success
+  end
 end
