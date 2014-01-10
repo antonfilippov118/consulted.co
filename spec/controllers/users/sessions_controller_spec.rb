07a2af6940041a0fc45c2bbbf2bf027bb5f26d97
Florian Kraft <f.kraft@ewerk.com>
2014-01-10 13:09:09 +0100

    [red]

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Users::SessionsController do
+  before(:each) do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+    User.delete_all
+  end
+  it 'allows a valid user to create a session' do
+    user = User.create email: 'florian@consulted.co', password: 'tester', password_confirmation: 'tester'
+    user.confirm!
 
+    post :create, email: 'florian@consulted.co', password: 'tester'
+
+    expect(response.success?).to be_true
+    expect(response.status).to eql 200
+  end
 end
