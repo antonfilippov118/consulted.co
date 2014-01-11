2e89a5d11fe7325c6023e59e9680f4588db428b8
Florian Kraft <f.kraft@ewerk.com>
2014-01-11 19:59:49 +0100

    [red]

--- spec/controllers/users/profile_controller_spec.rb
+++ spec/controllers/users/profile_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Users::ProfileController do
+  before(:each) do
+    User.delete_all
+    request.env['devise.mapping'] = Devise.mappings[:user]
+    user = User.create valid_params
+    sign_in user
+  end
 
+  it 'shows the users data' do
+    get :show
+
+    expect(response.succcess?).to be_true
+  end
+
+  def valid_params
+    {
+      email: 'florian@consulted.co',
+      name: 'Florian',
+      password: 'tester',
+      password_confirmation: 'tester'
+    }
+  end
 end
