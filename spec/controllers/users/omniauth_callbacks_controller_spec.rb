1347b4c51829a8bdb9228e6f5743fb16a19041ac
Florian Kraft <f.kraft@ewerk.com>
2014-01-12 11:52:06 +0100

    [green]

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Users::OmniauthCallbacksController do
+  before(:each) do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end
   it 'should be able to post to linkedin' do
     post :linkedin
   end
