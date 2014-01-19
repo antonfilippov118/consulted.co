00f22f667bb82735e5765e82020abcf92948935a
Florian Kraft <f.kraft@ewerk.com>
2014-01-19 22:00:35 +0100

    [green]

--- app/models/possible_time.rb
+++ app/models/possible_time.rb
@@ -2,5 +2,19 @@ class PossibleTime
   include Mongoid::Document
   belongs_to :user
 
-  field :length
+  field :length, type: Integer
+
+  validate :period_possible?
+
+  private
+
+  def possible_periods
+    [30, 45, 60, 90, 120]
+  end
+
+  def period_possible?
+    unless possible_periods.include? length
+      errors.add :time, 'is not a possible value'
+    end
+  end
 end
