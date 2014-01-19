5ecea52beb8b1b824657dcff25229559acbe9916
Florian Kraft <f.kraft@ewerk.com>
2014-01-19 22:14:23 +0100

    [red]

--- app/models/possible_time.rb
+++ app/models/possible_time.rb
@@ -3,23 +3,25 @@ class PossibleTime
   belongs_to :user
 
   field :length, type: Integer
-  field :weekday, type: Integer
+  field :weekday
 
-  validate :period_possible?
+  validate :length_possible?
+  validate :weekday_possible?
   validates_presence_of :user_id
 
   private
 
-  def possible_periods
+  def lengths_possible
     [30, 45, 60, 90, 120]
   end
 
-  def period_possible?
-    unless possible_periods.include? length
-      errors.add :time, 'is not a possible value'
+  def length_possible?
+    unless lengths_possible.include? length
+      errors.add :length, 'is not a possible value'
     end
   end
 
-  class Monday
+  def possible_weekdays
+    %w{Monday Tuesday Wednesday Thursday Friday Saturday Sunday}
   end
 end
