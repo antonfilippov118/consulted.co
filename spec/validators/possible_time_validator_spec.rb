4236d47b9ab9526a81ddc40964910a523d6b1ef4
Florian Kraft <f.kraft@ewerk.com>
2014-01-19 22:50:17 +0100

    [red]

--- spec/validators/possible_time_validator_spec.rb
+++ spec/validators/possible_time_validator_spec.rb
@@ -1,5 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PossibleTimeValidator do
+  let(:validator) { PossibleTimeValidator.new }
+  before(:all) do
+    User.delete_all
+  end
 
+  it 'should pass users without times scheduled' do
+    validator.validate user
+    expect(user.valid?).to be_true
+  end
+
+  def user
+    User.new name: 'florian', password: 'tester', password_confirmation: 'tester', confirmation_sent_at: Time.now
+  end
 end
