f64f6d988d79106cbc40ae30821b278a46d20860
Florian Kraft <f.kraft@ewerk.com>
2014-01-19 12:12:51 +0100

    [red]

--- spec/controllers/users/profile_controller_spec.rb
+++ spec/controllers/users/profile_controller_spec.rb
@@ -6,35 +6,49 @@ describe Users::ProfileController do
     User.delete_all
   end
 
-  it 'shows the users data when user is logged in' do
-    user = User.create valid_params
-    user.confirm!
-    sign_in user
+  context 'getting user data' do
+    it 'shows the users data when user is logged in' do
+      user = User.create valid_params
+      user.confirm!
+      sign_in user
 
-    get :show
+      get :show
 
-    expect(response.success?).to be_true
-    expect(response.status).to eql 200
+      expect(response.success?).to be_true
+      expect(response.status).to eql 200
+    end
+
+    it 'does not show the users data when signed out' do
+      get :show
+
+      expect(response.success?).to be_false
+      expect(response.status).to eql 401
+    end
+
+    it 'marks unconfirmed users with a flag' do
+      user = User.create valid_params
+      sign_in user
+
+      get :show
+
+      expect(response.success?).to be_true
+      json_hash = JSON.parse response.body
+
+      expect(json_hash).to have_key 'confirmed'
+      expect(json_hash['confirmend']).to be_false
+    end
   end
 
-  it 'does not show the users data when signed out' do
-    get :show
 
-    expect(response.success?).to be_false
-    expect(response.status).to eql 401
-  end
+  context 'updating data' do
+    it 'updates the user object' do
+      user = User.create valid_params
+      sign_in user
 
-  it 'marks unconfirmed users with a flag' do
-    user = User.create valid_params
-    sign_in user
+      patch :update, name: 'Flori'
 
-    get :show
-
-    expect(response.success?).to be_true
-    json_hash = JSON.parse response.body
-
-    expect(json_hash).to have_key 'confirmed'
-    expect(json_hash['confirmend']).to be_false
+      expect(user.name).to equal('Flori')
+    end
   end
 
   def valid_params
