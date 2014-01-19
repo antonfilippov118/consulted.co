63c58dde17f624663c71c2c808e38a4ac89daae4
Florian Kraft <f.kraft@ewerk.com>
2014-01-19 22:07:28 +0100

    [refactor] categorize the possible time spec

--- spec/models/possible_time_spec.rb
+++ spec/models/possible_time_spec.rb
@@ -2,31 +2,43 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe PossibleTime do
 
-  it 'should belong to a user' do
-    should belong_to(:user)
+  context 'creation' do
+    it 'should belong to a user' do
+      should belong_to(:user)
+    end
+
+    it 'has a length' do
+      expect do
+        PossibleTime.new.length = 120
+      end.not_to raise_error
+    end
   end
 
-  it 'has a length' do
-    expect do
-      PossibleTime.new.length = 120
-    end.not_to raise_error
+  context 'validation' do
+
+    it 'should only allow specific lengths' do
+      time = PossibleTime.new
+      time.length = 90
+
+      time.user = User.create name: 'Florian'
+      expect(time.valid?).to be_true
+
+      time = PossibleTime.new
+      time.user = User.create name: 'Florian'
+      time.length = 117
+
+      expect(time.valid?).to be_false
+    end
+
+    it 'should need a user' do
+      time = PossibleTime.new
+      time.length = 30
+      expect(time.valid?).to be_false
+    end
+
+    it 'should check against other times' do
+
+    end
   end
 
-  it 'should only allow specific lengths' do
-    time = PossibleTime.new
-    time.length = 90
-
-    expect(time.valid?).to be_true
-
-    time = PossibleTime.new
-    time.length = 117
-
-    expect(time.valid?).to be_false
-  end
-
-  it 'should need a user' do
-    time = PossibleTime.new
-    time.length = 30
-    expect(time.valid?).to be_false
-  end
 end
