97d40a15b255023b0d5b25e09c58cd7b91e7d797
Florian Kraft <f.kraft@ewerk.com>
2014-01-19 21:09:35 +0100

    [red]

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Category do
-
+  context 'when created' do
+    it 'should be able to have a name' do
+      expect do
+        Category.new.name = 'Foo'
+      end.not_to raise_error
+    end
+  end
 end
