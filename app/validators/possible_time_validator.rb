2d8d7531960e4f0dc639d95de8c7ee58fb1a7874
Florian Kraft <f.kraft@ewerk.com>
2014-01-24 12:16:29 +0100

    [refactor]

--- app/validators/possible_time_validator.rb
+++ app/validators/possible_time_validator.rb
@@ -5,7 +5,7 @@ class PossibleTimeValidator
 
   class CountValidator
     def validate(user)
-      if user.possible_times.count > maximum
+      if user.possible_times.count > maximum_per_week
         return false
       else
         return true
@@ -13,7 +13,7 @@ class PossibleTimeValidator
     end
 
     # TODO: make this configurable
-    def maximum
+    def maximum_per_week
       10
     end
   end
