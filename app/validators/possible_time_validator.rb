c9ca21239d20cff23b27ed272dade51a090eeef5
Florian Kraft <f.kraft@ewerk.com>
2014-01-24 16:09:42 +0100

    [green]

--- app/validators/possible_time_validator.rb
+++ app/validators/possible_time_validator.rb
@@ -1,21 +1,28 @@
 class PossibleTimeValidator < ActiveModel::Validator
   def validate(time)
+    return false if time.user.nil?
     ExpertValidator.validate time
     CountValidator.validate time
   end
 
   class ExpertValidator
     def self.validate(time)
+      return false unless user_confirmed? time
+      return false unless user_expert? time
+      true
+    end
 
-      if time.user.nil?
-        return false
-      end
+    private
 
+    def self.user_confirmed?(time)
       if time.user && !time.user.confirmed?
         time.errors.add :base, 'User must be confirmed!'
         return false
       end
+      true
+    end
 
+    def self.user_expert?(time)
       if time.user && !time.user.can_be_an_expert?
         time.errors.add :base, 'User must be an expert to save times!'
         return false
@@ -26,16 +33,36 @@ class PossibleTimeValidator < ActiveModel::Validator
 
   class CountValidator
     def self.validate(time)
+      return false if too_many?
+      return false if too_many_a_day? time
       true
     end
 
-    # TODO: make this configurable
-    def maximum_per_week
-      10
+    private
+
+    def self.too_many?
+      false
     end
 
-    def maximum_minutes_per_day
+    def self.too_many_a_day?(time)
+      minutes_today     = PossibleTime.for_user(time.user).on(time.weekday).in_week(time.week_of_year).sum(&:length)
+      minutes_recurring = PossibleTime.recurring.for_user(time.user).on(time.weekday).sum(&:length)
+      minutes_total     = minutes_recurring + minutes_today + time.length
+      if  minutes_total > maximum_minutes_per_day
+        time.errors.add :base, "This time cannot be created (#{minutes_total})"
+        return true
+      end
+      false
+    end
+
+    # TODO: make this configurable
+    def self.maximum_per_week
+      30
+    end
+
+    def self.maximum_minutes_per_day
       1440
     end
+
   end
 end
