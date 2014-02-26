eb5afb4fecf2741a3473e6b1c5462a88488ed923
Florian Kraft <f.kraft@ewerk.com>
2014-02-26 14:41:42 +0100

    remove last missing torquebox remnants

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,3 @@
 # encoding: utf-8
-# Configure the TorqueBox Servlet-based session store.
-# Provides for server-based, in-memory, cluster-compatible sessions
-if ENV['TORQUEBOX_APP_NAME']
-  Consulted::Application.config.session_store :torquebox_store
-else
-  key = 'dd6fe1a1784a861750a2cd3ca72cb66fb40aae2a954ad71112e5'
-  Consulted::Application.config.session_store :cookie_store, key: key
-end
+key = 'dd6fe1a1784a861750a2cd3ca72cb66fb40aae2a954ad71112e5'
+Consulted::Application.config.session_store :cookie_store, key: key
