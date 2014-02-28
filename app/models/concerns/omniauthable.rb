649b822877b3cfd5e0f246bab9a225fe91473be3
Florian Kraft <floriankraft@gmx.de>
2014-02-28 17:42:48 +0100

    cleanup

--- app/models/concerns/omniauthable.rb
+++ app/models/concerns/omniauthable.rb
@@ -11,33 +11,28 @@ module Omniauthable
 
   module Linkedin
     def connect_to_linkedin(auth)
-      if self.providers.nil?
-        self.providers = [auth.provider]
-      else
-        self.providers += [auth.provider]
-      end
+      self.providers = providers.nil? ? [auth.provider] : [auth.provider]
       self.uid       = auth.uid
       self.email     = auth.info.email
       self.name      = auth.info.name
-      self.password = Devise.friendly_token[0, 20] if self.password.nil?
+      self.password  = Devise.friendly_token[0, 20] if password.nil?
+
       self.user_linkedin_connection = User::LinkedinConnection.new(token: auth['extra']['access_token'].token, secret: auth['extra']['access_token'].secret)
 
-      unless save
-        return false
-      end
+      return false unless save
       synchronize_linkedin
       true
     end
 
     def disconnect_from_linkedin!
-      self.providers.delete "linkedin"
+      providers.delete 'linkedin'
       self.uid = nil
       self.user_linkedin_connection = nil
       save!
     end
 
     def synchronize_linkedin
-      result = SynchronizeLinkedinProfile.for id
+      SynchronizeLinkedinProfile.for id
     end
   end
 end
