e542ed9e9a5b59db13529ed7549eb011fbfb40c4
Florian Kraft <f.kraft@ewerk.com>
2014-03-03 17:05:37 +0100

    move offer model into user document

---
+++ app/actions/finds_available_experts.rb
@@ -0,0 +1,39 @@
+class FindsAvailableExperts
+  include LightService::Organizer
+
+  def self.for(group)
+    with(group: group).reduce [
+      FindAvailableUsers,
+      FilterExpertsWithTime,
+      RankExperts
+    ]
+  end
+
+  class FindAvailableUsers
+    include LightService::Action
+    executed do |context|
+      group   = context.fetch :group
+      unless group.class == Array
+        group = [group]
+      end
+      experts = User.experts.with_groups group
+      context[:experts] = experts
+    end
+  end
+
+  class FilterExpertsWithTime
+    include LightService::Action
+
+    executed do |context|
+      # TODO: implement this, when bookings are actually possible to filter experts with no time
+    end
+  end
+
+  class RankExperts
+    include LightService::Action
+
+    executed do |context|
+      # TODO: implement a ranking system
+    end
+  end
+end
