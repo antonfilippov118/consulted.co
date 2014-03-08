e31de36d8c74e26601ac9720b461e9b91d17b307
Marco Wegner <acrolyt@gmail.com>
2014-03-08 20:59:26 +0100

    Fav weiter

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,14 +1,7 @@
 class User
   include Mongoid::Document
 
-  field :user_id, type: Int
-  field :summary, type: String
-  field :newsletter, type: Boolean
-  field :languages, type: Array, default: ['english']
-  field :slug, type: String
-  field :timezone, type: String, default: 'Europe/Berlin'
-
-  field :providers, type: Array
-
+  field :user_id, type: Integer
+  field :fav_user_id, type: Integer
 
 end
