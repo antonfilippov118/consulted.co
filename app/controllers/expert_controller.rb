f74ad12bfc88810a6be8eb057b7ed2e9f02e09e1
Florian Kraft <floriankraft@gmx.de>
2014-03-09 00:05:47 +0100

    add basic expert page

--- app/controllers/expert_controller.rb
+++ app/controllers/expert_controller.rb
@@ -1,7 +1,11 @@
 class ExpertController < ApplicationController
   rescue_from Mongoid::Errors::DocumentNotFound, with: :render404
   def page
-    @expert = User.experts.find_by slug: params[:slug]
+    user = User.experts.find_by slug: params[:slug]
+    unless user.can_be_an_expert?
+      return render404
+    end
+    @expert = user
   end
 
   def render404
