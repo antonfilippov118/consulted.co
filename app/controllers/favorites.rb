8f2ca1eb4a8ea72f3c26c2a0b1ee55ff6792b78a
Marco Wegner <acrolyt@gmail.com>
2014-03-11 19:57:05 +0100

    Before Rebase

--- app/controllers/favorites.rb
+++ app/controllers/favorites.rb
@@ -1,7 +1,23 @@
 class FavoritesController < ApplicationController
 
+  def favorite
+    type = params[:type]
+    if type == 'favorite'
+      current_user.favorites << @user
+      redirect_to :back, notice: 'You favorited #{@user.name}'
+
+    elsif type == 'unfavorite'
+      current_user.favorites.delete(@user)
+      redirect_to :back, notice: 'Unfavorited #{@user.name}'
+
+    else
+      # Type missing, nothing happens
+      redirect_to :back, notice: 'Nothing happened.'
+    end
+  end
+
   def index
-    @groups = Group.roots
+
   end
 
   def show
