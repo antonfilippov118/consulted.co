860a8224f4b143ea929d3f099815b836f96cd91a
Anton Gudkov <anton.gudkov@flatstack.com>
2014-03-19 02:29:20 +0400

    Disable the frontend view for groups which are not service offerings. Improve controller spec.

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,13 +1,48 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require 'spec_helper'
 
 describe GroupsController do
-  before(:each) do
-    request.accept = 'application/json'
+  let(:group) { build :group }
+  let(:groups) { [group] }
+
+  before do
+    Group.stub(:roots) { groups }
+    Group.stub(:find) { group }
   end
-  it 'shows the groups as json' do
-    Group.delete_all
-    Group.create name: 'Finance'
-    get :index
-    expect(response.success?).to be_true
+
+  describe 'GET #index' do
+    before do
+      get :index, format: :json
+    end
+
+    it 'shows the groups as json' do
+      expect(response).to be_success
+    end
+  end
+
+  describe 'GET #show' do
+    context 'when group has no children' do
+      before do
+        get :show, id: 'id'
+      end
+
+      it 'responds successfully' do
+        expect(response).to be_success
+      end
+
+      it 'assigns group' do
+        expect(assigns(:group)).to eq(group)
+      end
+    end
+
+    context 'when group has children' do
+      before do
+        group.stub(:children?) { true }
+        get :show, id: 'id'
+      end
+
+      it 'should redirect to search' do
+        expect(response).to redirect_to(search_path)
+      end
+    end
   end
 end
