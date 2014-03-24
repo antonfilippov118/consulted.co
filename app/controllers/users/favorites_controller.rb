8462a329255d91e5d8faa5510025fcef267a8291
Marco Wegner <acrolyt@gmail.com>
2014-03-24 22:43:23 +0100

    Favo geht

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -2,18 +2,19 @@ class Users::FavoritesController < Users::BaseController
 
   def index
 
-    @favorites = @user.favorites
+    #@favorites = @user.favorites
+    @favorites = User.find @user.favorites.map(&:favorite_id)
   end
 
   def update
     id = params[:id]
     expert = User.find(id)
-    @fav = @user.favorites.where(user_id: id)
+    @fav = @user.favorites.where(favorite_id: id)
 
     if @fav.exists? == true
       @fav.destroy
     else
-      @user.favorites.new user: expert, user_id: id
+      @user.favorites.new user: expert, favorite_id: id
       @user.save
     end
     # binding.pry
