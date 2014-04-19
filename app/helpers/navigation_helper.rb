594706e9170ec7da94112e25fe34c70dd5be6ff7
Florian Kraft <floriankraft@gmx.de>
2014-04-19 22:38:59 +0200

    hides headers and menus on certain pages
    closes https://app.asana.com/0/11057468897092/11761115564136

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -7,4 +7,16 @@ module NavigationHelper
   def show_signup?
     !user_signed_in? && !current_page?(new_user_registration_path) && !current_page?(new_user_session_path)
   end
+
+  def login_view?
+    registrations = controller_name == 'registrations' && action_name == 'create'
+    login         = controller_name == 'sessions' && action_name == 'create'
+    pages = [
+      new_user_registration_path,
+      new_user_session_path
+    ].map do |page|
+      current_page? page
+    end
+    pages.include?(true) || login || registrations
+  end
 end
