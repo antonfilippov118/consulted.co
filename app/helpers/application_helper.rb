a0a2926dce6c84d7250fedbc5c382034c1c342f8
Florian Kraft <floriankraft@gmx.de>
2014-05-12 17:15:46 +0200

    remove analytis from actual codebase
    (GA will be served by cloudflare)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,6 @@
 # encoding: utf-8
 
 module ApplicationHelper
-  def analytics_key
-    ENV['ANALYTICS_KEY']
-  end
 
   def determined_path(resource)
     if resource.sign_in_count == 1 && !resource.providers.nil?
