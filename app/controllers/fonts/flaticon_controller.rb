d1b4a741afb63c5e7923b1239dd3a5413931b0fc
Florian Kraft <floriankraft@gmx.de>
2014-05-21 10:35:07 +0200

    cache font files

--- app/controllers/fonts/flaticon_controller.rb
+++ app/controllers/fonts/flaticon_controller.rb
@@ -1,16 +1,23 @@
 class Fonts::FlaticonController < ApplicationController
   respond_to :woff, :ttf, :eot, :svg
   def show
+    response.headers['Expires'] = cache_time.from_now.httpdate
+    response.headers['Etag'] = Digest::SHA1.hexdigest(font_file.path)
+    expires_in cache_time, public: true
     send_file font_file
   end
 
   private
 
   def font_file
-    Rails.root.join('app', 'assets', 'fonts', "flaticon.#{format}")
+    File.new(Rails.root.join('app', 'assets', 'fonts', "flaticon.#{format}"), 'r')
   end
 
   def format
     params[:format] || 'woff'
+  end
+
+  def cache_time
+    30.days
   end
 end
