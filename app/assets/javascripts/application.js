47bbb604f9af56c04a3aaf23f612b97e50766180
Philipp Klose <me@thehippo.de>
2014-05-26 22:28:38 +0200

    moar removing of dxhtml scheduler
    Signed-off-by: Philipp Klose <me@thehippo.de>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,4 @@
 //= require angular-typeahead/angular-typeahead
 //= require noty/js/noty/packaged/jquery.noty.packaged.min
 //= require ion-range-slider/js/ion.rangeSlider.js
-//= require dhtmlx/dhtmlxscheduler
-//= require dhtmlx/ext/dhtmlxscheduler_collision
-//= require dhtmlx/ext/dhtmlxscheduler_limit
 //= require_tree ./consulted
