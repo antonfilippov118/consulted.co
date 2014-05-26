029bf7e9c0608190d4c1a3be2af12803dcf8d730
Florian Kraft <floriankraft@gmx.de>
2014-05-26 20:15:52 +0200

    typo

--- app/models/concerns/liquidatable/user.rb
+++ app/models/concerns/liquidatable/user.rb
@@ -4,12 +4,13 @@ module Liquidatable
 
     def to_liquid
       {
-        'name'       => name,
-        'first_name' => first_name,
-        'last_name'  => last_name,
-        'email'      => email,
-        'linkedin'   => linkedin_url,
-        'twitter'    => twitter_url
+        'name'              => name,
+        'first_name'        => first_name,
+        'last_name'         => last_name,
+        'email'             => email,
+        'linkedin'          => linkedin_url,
+        'twitter'           => twitter_url,
+        'notification_time' => notification_time
       }
     end
   end
