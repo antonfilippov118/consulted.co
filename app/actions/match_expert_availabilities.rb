d4037a32ad291a6daa45a9f6c7f88a0073ced9e7
Florian Kraft <floriankraft@gmx.de>
2014-05-26 22:44:37 +0200

    add a debug call

--- app/actions/match_expert_availabilities.rb
+++ app/actions/match_expert_availabilities.rb
@@ -73,6 +73,7 @@ class MatchExpertAvailabilities
         ids = experts_available.map { |expert| expert.id }
       rescue => e
         context.fail! e.message
+        logger.debug e.message
         ids = []
       end
 
