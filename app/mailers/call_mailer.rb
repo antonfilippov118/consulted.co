3667ec6aef65671488d0d8e3d9337a65363b8773
Florian Kraft <floriankraft@gmx.de>
2014-05-28 18:59:10 +0200

    include the timezone for call emails

--- app/mailers/call_mailer.rb
+++ app/mailers/call_mailer.rb
@@ -1,51 +1,51 @@
 class CallMailer < ApplicationMailer
 
   def seeker_confirmation(call)
-    liquid_mail(:call_final_confirmation_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker))
+    liquid_mail(:call_final_confirmation_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker), timezone: timezone(call, :seeker))
   end
 
   def expert_confirmation(call)
-    liquid_mail(:call_final_confirmation_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert))
+    liquid_mail(:call_final_confirmation_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert), timezone: timezone(call, :expert))
   end
 
   def call_declined_by_expert_to_seeker(call)
-    liquid_mail(:call_declined_by_expert_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker))
+    liquid_mail(:call_declined_by_expert_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker), timezone: timezone(call, :seeker))
   end
 
   def call_declined_by_expert_manually(call)
-    liquid_mail(:call_declined_by_expert_manually, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert))
+    liquid_mail(:call_declined_by_expert_manually, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert), timezone: timezone(call, :seeker))
   end
 
   def call_declined_by_expert_auto(call)
-    liquid_mail(:call_declined_by_expert_auto, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert))
+    liquid_mail(:call_declined_by_expert_auto, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert), timezone: timezone(call, :seeker))
   end
 
   def call_reminder_to_seeker(call)
-    liquid_mail(:call_reminder_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker))
+    liquid_mail(:call_reminder_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker), timezone: timezone(call, :seeker))
   end
 
   def call_reminder_to_expert(call)
-    liquid_mail(:call_reminder_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert))
+    liquid_mail(:call_reminder_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert), timezone: timezone(call, :expert))
   end
 
   def call_followup_to_seeker(call)
-    liquid_mail(:call_followup_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker))
+    liquid_mail(:call_followup_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker), timezone: timezone(call, :seeker))
   end
 
   def call_cancelled_by_seeker_to_seeker(call)
-    liquid_mail(:call_cancelled_by_seeker_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker))
+    liquid_mail(:call_cancelled_by_seeker_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker), timezone: timezone(call, :seeker))
   end
 
   def call_cancelled_by_seeker_to_expert(call)
-    liquid_mail(:call_cancelled_by_seeker_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert))
+    liquid_mail(:call_cancelled_by_seeker_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert), timezone: timezone(call, :expert))
   end
 
   def call_cancelled_by_expert_to_seeker(call)
-    liquid_mail(:call_cancelled_by_expert_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker))
+    liquid_mail(:call_cancelled_by_expert_to_seeker, { to: call.seeker.notification_email }, user: call.seeker, call: call, date: date(call, :seeker), timezone: timezone(call, :seeker))
   end
 
   def call_cancelled_by_expert_to_expert(call)
-    liquid_mail(:call_cancelled_by_expert_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert))
+    liquid_mail(:call_cancelled_by_expert_to_expert, { to: call.expert.notification_email }, user: call.expert, call: call, date: date(call, :expert), timezone: timezone(call, :expert))
   end
 
   private
@@ -59,5 +59,10 @@ class CallMailer < ApplicationMailer
       date = call.active_to.utc
     end
     date.strftime '%A, %B %-d %Y, %H:%M'
+  end
+
+  def timezone(call, partner)
+    partner = call.send partner
+    ActiveSupport::TimeZone.new(partner.timezone).to_s
   end
 end
