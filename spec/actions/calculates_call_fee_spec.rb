893b46594153b206e1f69bc09b1c0aa1174a3acb
Florian Kraft <floriankraft@gmx.de>
2014-05-29 00:08:31 +0200

    WIP

--- spec/actions/calculates_call_fee_spec.rb
+++ spec/actions/calculates_call_fee_spec.rb
@@ -5,5 +5,8 @@ class CalculatesCallFee
 end
 
 describe CalculatesCallFee do
-
+  let(:action) { CalculatesCallFee }
+  it 'should not calculate a fee for any call other than cancelled calls' do
+    pending
+  end
 end
