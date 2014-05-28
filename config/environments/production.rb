6d56de2fbc011c43e5fb592eb9266970a7681f5b
Florian Kraft <floriankraft@gmx.de>
2014-05-28 11:34:42 +0200

    correct options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,8 @@ Consulted::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options[:protocol] = :https
-  config.action_mailer.default_url_options[:host] = 'beta.consulted.co'
+  config.action_mailer.default_url_options = {
+    protocol: :https,
+    host: 'beta.consulted.co'
+  }
 end
