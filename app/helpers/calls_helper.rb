4d6b68aea4bffa3252dd08bc081fa55e7b398cf0
Florian Kraft <floriankraft@gmx.de>
2014-06-04 10:53:21 +0200

    correctly display call status for seeker after cancellation
    https://app.asana.com/0/9734630424771/12751179429308

--- app/helpers/calls_helper.rb
+++ app/helpers/calls_helper.rb
@@ -68,7 +68,7 @@ module CallsHelper
       Call::Status::REQUESTED => 'Awaiting confirmation',
       Call::Status::ACTIVE => 'Scheduled',
       Call::Status::CANCELLED => 'Cancelled',
-      Call::Status::DECLINED => 'Declined'
+      Call::Status::DECLINED => 'Cancelled'
     }
   end
 
